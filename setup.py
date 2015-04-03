df160ec4e7dfe8e5ced323ab6fd11213efb2d94e
Jimmy Callin <jimmy.callin@gmail.com>
2015-04-03 17:12:23 +0200

    updated readme and setup.py

--- setup.py
+++ setup.py
@@ -19,5 +19,6 @@ setuptools.setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.4',
-    ]
+    ],
+    install_requires=['pymysql']
 )
